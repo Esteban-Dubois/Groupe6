0cde18cae14506e0d9b7480758aac52ce9022755
Esteban-Dubois <esteban.dubois@edu.univ-fcomte.fr>
2026-01-30 08:36:36 +0100

    Ajout du dossiers Controllers

---
+++ controllers/admin_article.py
@@ -0,0 +1,225 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+import math
+import os.path
+from random import random
+
+from flask import Blueprint
+from flask import request, render_template, redirect, flash
+#from werkzeug.utils import secure_filename
+
+from connexion_db import get_db
+
+admin_article = Blueprint('admin_article', __name__,
+                          template_folder='templates')
+
+
+@admin_article.route('/admin/article/show')
+def show_article():
+    mycursor = get_db().cursor()
+    sql = '''  
+    SELECT
+    fusee.id_fusee AS id_article,
+    fusee.nom_fusee AS nom,
+    fusee.categorie_id AS type_article_id,
+    fusee.prix_fusee AS prix,
+    fusee.stock_fusee AS stock,
+    fusee.image_fusee AS image,
+    categorie.libelle_categorie AS libelle
+    FROM fusee
+    JOIN categorie ON categorie.id_categorie = fusee.categorie_id;
+    '''
+    mycursor.execute(sql)
+    articles = mycursor.fetchall()
+    return render_template('admin/article/show_article.html', articles=articles)
+
+
+@admin_article.route('/admin/article/add', methods=['GET'])
+def add_article():
+    mycursor = get_db().cursor()
+    sql = '''
+    SELECT
+    id_categorie AS id_type_article,
+    libelle_categorie AS libelle
+    FROM categorie;
+    '''
+    mycursor.execute(sql)
+    type_article = mycursor.fetchall()
+
+    return render_template('admin/article/add_article.html'
+                           ,types_article=type_article#,
+                           #,couleurs=colors
+                           #,tailles=tailles
+                            )
+
+
+@admin_article.route('/admin/article/add', methods=['POST'])
+def valid_add_article():
+    mycursor = get_db().cursor()
+
+    nom = request.form.get('nom', '')
+    type_article_id = request.form.get('type_article_id', '')
+    prix = request.form.get('prix', '')
+    description = request.form.get('description', '')
+    image = request.files.get('image', '')
+
+    if image:
+        filename = 'img_upload'+ str(int(2147483647 * random())) + '.png'
+        image.save(os.path.join('static/images/', filename))
+    else:
+        print("erreur")
+        filename=None
+
+    sql = ''' INSERT INTO fusee (nom_fusee, image_fusee, prix_fusee, categorie_id, description_fusee) VALUES (%s, %s, %s, %s, %s); '''
+
+    tuple_add = (nom, filename, prix, type_article_id, description)
+    print(tuple_add)
+    mycursor.execute(sql, tuple_add)
+    get_db().commit()
+
+    print(u'article ajouté , nom: ', nom, ' - type_article:', type_article_id, ' - prix:', prix,
+          ' - description:', description, ' - image:', image)
+    message = u'article ajouté , nom:' + nom + '- type_article:' + type_article_id + ' - prix:' + prix + ' - description:' + description + ' - image:' + str(
+        image)
+    flash(message, 'alert-success')
+    return redirect('/admin/article/show')
+
+
+@admin_article.route('/admin/article/delete', methods=['GET'])
+def delete_article():
+    id_article=request.args.get('id_article')
+    mycursor = get_db().cursor()
+    sql = "SELECT COUNT(*) AS nb_declinaison FROM ligne_panier WHERE fusee_id_panier = %s;"
+    mycursor.execute(sql, (id_article,))
+    nb_declinaison = mycursor.fetchone()
+    if nb_declinaison['nb_declinaison'] > 0:
+        message= u'il y a des declinaisons dans cet article : vous ne pouvez pas le supprimer'
+        flash(message, 'alert-warning')
+    else:
+        sql = "SELECT image_fusee AS image FROM fusee WHERE id_fusee = %s;"
+        mycursor.execute(sql, (id_article,))
+        article = mycursor.fetchone()
+        print(article)
+        image = article['image']
+
+        sql = "DELETE FROM fusee WHERE id_fusee = %s;"
+        mycursor.execute(sql, (id_article,))
+        get_db().commit()
+        if image != None:
+            os.remove('static/images/' + image)
+
+        print("un article supprimé, id :", id_article)
+        message = u'un article supprimé, id : ' + id_article
+        flash(message, 'alert-success')
+
+    return redirect('/admin/article/show')
+
+
+@admin_article.route('/admin/article/edit', methods=['GET'])
+def edit_article():
+    id_article=request.args.get('id_article')
+    mycursor = get_db().cursor()
+    sql = '''
+    SELECT 
+    id_fusee AS id_article, 
+    nom_fusee AS nom, 
+    prix_fusee AS prix, 
+    categorie_id AS type_article_id, 
+    description_fusee AS description, 
+    image_fusee AS image
+    FROM fusee 
+    WHERE id_fusee = %s;
+    '''
+    mycursor.execute(sql, (id_article,))
+    article = mycursor.fetchone()
+    print(article)
+    sql = '''
+    SELECT 
+    id_categorie AS id_type_article, 
+    libelle_categorie AS libelle 
+    FROM categorie;
+    '''
+    mycursor.execute(sql)
+    types_article = mycursor.fetchall()
+
+    # sql = '''
+    # requête admin_article_6
+    # '''
+    # mycursor.execute(sql, id_article)
+    # declinaisons_article = mycursor.fetchall()
+
+    return render_template('admin/article/edit_article.html'
+                           ,article=article
+                           ,types_article=types_article
+                         #  ,declinaisons_article=declinaisons_article
+                           )
+
+
+@admin_article.route('/admin/article/edit', methods=['POST'])
+def valid_edit_article():
+    mycursor = get_db().cursor()
+    nom = request.form.get('nom')
+    id_article = request.form.get('id_article')
+    image = request.files.get('image', '')
+    type_article_id = request.form.get('type_article_id', '')
+    prix = request.form.get('prix', '')
+    description = request.form.get('description')
+    sql = '''
+       SELECT image_fusee AS image FROM fusee WHERE id_fusee = %s;
+       '''
+    mycursor.execute(sql, (id_article,))
+    image_nom = mycursor.fetchone()
+    image_nom = image_nom['image']
+    if image:
+        if image_nom != "" and image_nom is not None and os.path.exists(
+                os.path.join(os.getcwd() + "/static/images/", image_nom)):
+            os.remove(os.path.join(os.getcwd() + "/static/images/", image_nom))
+        # filename = secure_filename(image.filename)
+        if image:
+            filename = 'img_upload_' + str(int(2147483647 * random())) + '.png'
+            image.save(os.path.join('static/images/', filename))
+            image_nom = filename
+
+    sql = '''  
+    UPDATE fusee 
+    SET 
+    nom_fusee = %s, 
+    image_fusee = %s, 
+    prix_fusee = %s, 
+    categorie_id = %s, 
+    description_fusee = %s 
+    WHERE id_fusee = %s; 
+    '''
+    mycursor.execute(sql, (nom, image_nom, prix, type_article_id, description, id_article))
+
+    get_db().commit()
+    if image_nom is None:
+        image_nom = ''
+    message = u'article modifié , nom:' + nom + '- type_article :' + type_article_id + ' - prix:' + prix  + ' - image:' + image_nom + ' - description: ' + description
+    flash(message, 'alert-success')
+    return redirect('/admin/article/show')
+
+
+
+
+
+
+
+@admin_article.route('/admin/article/avis/<int:id>', methods=['GET'])
+def admin_avis(id):
+    mycursor = get_db().cursor()
+    article=[]
+    commentaires = {}
+    return render_template('admin/article/show_avis.html'
+                           , article=article
+                           , commentaires=commentaires
+                           )
+
+
+@admin_article.route('/admin/comment/delete', methods=['POST'])
+def admin_avis_delete():
+    mycursor = get_db().cursor()
+    article_id = request.form.get('idArticle', None)
+    userId = request.form.get('idUser', None)
+
+    return admin_avis(article_id)
---
+++ controllers/admin_type_article.py
@@ -0,0 +1,91 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+from flask import Blueprint
+from flask import Flask, request, render_template, redirect, flash, session
+
+from connexion_db import get_db
+
+admin_type_article = Blueprint('admin_type_article', __name__,
+                        template_folder='templates')
+
+@admin_type_article.route('/admin/type-article/show')
+def show_type_article():
+    mycursor = get_db().cursor()
+    sql = ''' 
+    SELECT 
+    categorie.id_categorie AS id_type_article,
+    categorie.libelle_categorie AS libelle,
+    COUNT(fusee.id_fusee) AS nbr_articles
+    FROM categorie
+    LEFT JOIN fusee ON categorie.id_categorie = fusee.categorie_id
+    GROUP BY categorie.id_categorie, categorie.libelle_categorie;
+    '''
+    mycursor.execute(sql)
+    types_article = mycursor.fetchall()
+    #types_article=[]
+    return render_template('admin/type_article/show_type_article.html', types_article=types_article)
+
+@admin_type_article.route('/admin/type-article/add', methods=['GET'])
+def add_type_article():
+    return render_template('admin/type_article/add_type_article.html')
+
+@admin_type_article.route('/admin/type-article/add', methods=['POST'])
+def valid_add_type_article():
+    libelle = request.form.get('libelle', '')
+    tuple_insert = (libelle,)
+    mycursor = get_db().cursor()
+    sql = ''' 
+    INSERT INTO categorie (libelle_categorie) VALUES (%s);
+    '''
+    mycursor.execute(sql, tuple_insert)
+    get_db().commit()
+    message = u'type ajouté , libellé :'+libelle
+    flash(message, 'alert-success')
+    return redirect('/admin/type-article/show') #url_for('show_type_article')
+
+@admin_type_article.route('/admin/type-article/delete', methods=['GET'])
+def delete_type_article():
+    id_type_article = request.args.get('id_type_article', '')
+    mycursor = get_db().cursor()
+
+    flash(u'suppression type article , id : ' + id_type_article, 'alert-success')
+    return redirect('/admin/type-article/show')
+
+@admin_type_article.route('/admin/type-article/edit', methods=['GET'])
+def edit_type_article():
+    id_type_article = request.args.get('id_type_article', '')
+    mycursor = get_db().cursor()
+    sql = ''' 
+    SELECT 
+    id_categorie AS id_type_article,
+    libelle_categorie AS libelle
+    FROM categorie
+    WHERE id_categorie = %s;
+    '''
+    mycursor.execute(sql, (id_type_article,))
+    type_article = mycursor.fetchone()
+    return render_template('admin/type_article/edit_type_article.html', type_article=type_article)
+
+@admin_type_article.route('/admin/type-article/edit', methods=['POST'])
+def valid_edit_type_article():
+    libelle = request.form['libelle']
+    id_type_article = request.form.get('id_type_article', '')
+    tuple_update = (libelle, id_type_article)
+    mycursor = get_db().cursor()
+    sql = ''' 
+    UPDATE categorie 
+    SET libelle_categorie = %s 
+    WHERE id_categorie = %s;
+    '''
+    mycursor.execute(sql, tuple_update)
+    get_db().commit()
+    flash(u'type article modifié, id: ' + id_type_article + " libelle : " + libelle, 'alert-success')
+    return redirect('/admin/type-article/show')
+
+
+
+
+
+
+
+
