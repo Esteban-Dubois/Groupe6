96eb70ca7c47aedc7d5da312609a0537149c0a6c
Esteban-Dubois <esteban.dubois@edu.univ-fcomte.fr>
2026-02-01 12:06:16 +0100

    dotenv pour la secret key

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 from flask import Blueprint
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 from controllers.auth_security import *
@@ -23,7 +26,7 @@ from controllers.admin_commentaire import *
 from controllers.client_liste_envies import *
 
 app = Flask(__name__)
-app.secret_key = 'une cle(token) : grain de sel(any random string)'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 @app.teardown_appcontext
