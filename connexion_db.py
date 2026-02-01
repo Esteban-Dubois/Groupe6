41467cbee3006b7883b6acd50211d5d33968544f
Esteban-Dubois <esteban.dubois@edu.univ-fcomte.fr>
2026-02-01 15:31:54 +0100

    Update connexion_db.py

--- connexion_db.py
+++ connexion_db.py
@@ -24,6 +24,7 @@ def get_db():
                 user=os.environ.get('DB_USER'),
                 password=os.environ.get('DB_PASSWORD'),
                 database=os.environ.get('DB_NAME'),
+                port=int(os.environ.get('DB_PORT', 3306)),
                 charset='utf8mb4',
                 cursorclass=pymysql.cursors.DictCursor
             )
