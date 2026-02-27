0959885eb3e894949ecd93a19cab7529abd5c0e8
kylliandonguy <kyllian.donguy@edu.univ-fcomte.fr>
2026-02-27 12:46:32 +0100

    ajout du filtre fonctionnelle

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -9,36 +9,60 @@ client_article = Blueprint('client_article', __name__,
                         template_folder='templates')
 
 @client_article.route('/client/index')
-@client_article.route('/client/article/show')              # remplace /client
-def client_article_show():                                 # remplace client_index
+@client_article.route('/client/article/show')
+def client_article_show():
     mycursor = get_db().cursor()
     id_client = session['id_user']
 
-    sql = '''   SELECT id_fusee AS id_article,
-    prix_fusee AS prix, 
-    stock_fusee AS stock,
-    nom_fusee AS nom,
-    image_fusee AS image, 
-    categorie.libelle_categorie AS libelle
-    FROM fusee
-    JOIN categorie ON fusee.categorie_id = categorie.id_categorie;'''
-    mycursor.execute(sql)
+    # 1. Récupération des filtres depuis la session
+    word = session.get('filter_word', '')
+    p_min = session.get('filter_prix_min', '')
+    p_max = session.get('filter_prix_max', '')
+    types = session.get('filter_types', [])
+
+    # 2. SQL des articles (Catalogue)
+    sql_articles = '''   
+        SELECT id_fusee AS id_article,
+               prix_fusee AS prix, 
+               stock_fusee AS stock,
+               nom_fusee AS nom,
+               image_fusee AS image, 
+               categorie.libelle_categorie AS libelle
+        FROM fusee
+        JOIN categorie ON fusee.categorie_id = categorie.id_categorie
+        WHERE 1=1 
+    '''
+    params_articles = []
+
+    if word:
+        sql_articles += " AND nom_fusee LIKE %s"
+        params_articles.append(f"%{word}%")
+    if p_min:
+        sql_articles += " AND prix_fusee >= %s"
+        params_articles.append(p_min)
+    if p_max:
+        sql_articles += " AND prix_fusee <= %s"
+        params_articles.append(p_max)
+    if types:
+        placeholders = ', '.join(['%s'] * len(types))
+        sql_articles += f" AND fusee.categorie_id IN ({placeholders})"
+        params_articles.extend(types)
+
+    mycursor.execute(sql_articles, tuple(params_articles))
     articles = mycursor.fetchall()
     
-    sql = 'SELECT libelle_categorie AS libelle FROM categorie'
-    mycursor.execute(sql)
-    libelle_articles = mycursor.fetchall()
-    list_param = []
-    condition_and = ""
-    # utilisation du filtre
-    sql3=''' prise en compte des commentaires et des notes dans le SQL    '''
-    #articles =[]
+    # 3. SQL des types UNIQUE (SANS DOUBLONS)
+    sql_types_filtre = '''
+        SELECT DISTINCT id_categorie AS id_type_article, 
+                        libelle_categorie AS libelle 
+        FROM categorie
+        ORDER BY libelle ASC
+    '''
+    mycursor.execute(sql_types_filtre)
+    types_article = mycursor.fetchall() 
 
-
-    # pour le filtre
-    types_article = []
-
-    sql = '''
+    # 4. SQL du panier
+    sql_panier = '''
         SELECT f.nom_fusee AS nom, 
                lp.quantite AS quantite, 
                f.prix_fusee AS prix, 
@@ -48,24 +72,29 @@ def client_article_show():                                 # remplace client_ind
         JOIN fusee f ON lp.fusee_id_panier = f.id_fusee
         WHERE lp.utilisateur_id_panier = %s
     '''
-    
-    mycursor.execute(sql, (id_client,)) 
+    mycursor.execute(sql_panier, (id_client,)) 
     articles_panier = mycursor.fetchall()
 
+    # 5. Calcul du total
     if len(articles_panier) >= 1:
-        sql = ''' SELECT SUM(ligne_panier.quantite * fusee.prix_fusee) AS total
-                  FROM fusee
-                  JOIN ligne_panier ON fusee.id_fusee = ligne_panier.fusee_id_panier
-                  WHERE ligne_panier.utilisateur_id_panier = %s
+        sql_total = ''' 
+            SELECT SUM(ligne_panier.quantite * fusee.prix_fusee) AS total
+            FROM fusee
+            JOIN ligne_panier ON fusee.id_fusee = ligne_panier.fusee_id_panier
+            WHERE ligne_panier.utilisateur_id_panier = %s
         '''
-        mycursor.execute(sql, (id_client,))
-        prix_total = mycursor.fetchone()['total']
+        mycursor.execute(sql_total, (id_client,))
+        res_total = mycursor.fetchone()
+        prix_total = res_total['total'] if res_total else 0
     else:
         prix_total = None
-    return render_template('client/boutique/panier_article.html'
-                           , articles=articles
-                           , articles_panier=articles_panier
-                           , prix_total=prix_total
-                           , items_filtre=types_article
-                           , libelle_articles=libelle_articles
+
+    # --- LA CORRECTION POUR LES DOUBLONS ---
+    # Ton HTML a deux boucles. On en "neutralise" une en envoyant une liste vide.
+    return render_template('client/boutique/panier_article.html',
+                           articles=articles,
+                           articles_panier=articles_panier,
+                           prix_total=prix_total,
+                           items_filtre=types_article, # Cette liste sera affichée
+                           libelle_articles=[]          # On envoie du vide ici pour supprimer le doublon
                            )
